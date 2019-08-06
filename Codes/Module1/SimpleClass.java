14891f200256cc81ed6a1909e8b4f6a1f16e753f
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-06 08:09:15 +0530

    Codes, Module1, SimpleClass.java: Add a Java file to demonstrate the declaration of classes in Java.

---
+++ Codes/Module1/SimpleClass.java
@@ -0,0 +1,16 @@
+/**
+ * @author Divyansh Bhardwaj
+ * A simple Java code to demonstrate a class.
+ * */
+public class SimpleClass { // this is the declaration of a class
+
+}
+
+/*
+* We can have multiple classes inside a Java file, but then, only one of them can be
+* public. Also, if a class is public inside a Java file, then, the name of the Java file
+* should exactly match the name of the file.
+* */
+class SecondClass {
+
+}
