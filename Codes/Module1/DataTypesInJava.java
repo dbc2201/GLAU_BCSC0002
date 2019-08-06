9b9434bc678ebbfa239c5d872e437adf0f28fe46
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-06 08:12:09 +0530

    Codes, Module1, DataTypesInJava.java: add comment for names of data types

--- Codes/Module1/DataTypesInJava.java
+++ Codes/Module1/DataTypesInJava.java
@@ -3,6 +3,16 @@
  * */
 public class DataTypesInJava {
 	public static void main(String[] args) {
-
+		/*
+		* Java supports 8 different primitive data types, namely -
+		* 1. byte
+		* 2. short
+		* 3. char
+		* 4. int
+		* 5. long
+		* 6. float
+		* 7. double
+		* 8. boolean
+		* */
 	}
 }
