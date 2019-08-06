bc2f6431266680f8c5ec4d692f72bc15d454dca1
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-06 08:25:23 +0530

    Codes, Module1, SimpleClass.java: fix typo for public class naming rule

--- Codes/Module1/SimpleClass.java
+++ Codes/Module1/SimpleClass.java
@@ -9,7 +9,7 @@ public class SimpleClass { // this is the declaration of a class
 /*
 * We can have multiple classes inside a Java file, but then, only one of them can be
 * public. Also, if a class is public inside a Java file, then, the name of the Java file
-* should exactly match the name of the file.
+* should exactly match the name of the public class.
 * */
 class SecondClass {
 
