0651dc08878698d0c5c6fb19b535231ff3606bf2
dbc2201 <divyansh.bhardwaj@gla.ac.in>
2019-08-06 08:06:10 +0530

    Codes, Module1, HelloWorld.java: add a simple "Hello, World!" program in Java.

---
+++ Codes/Module1/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author Divyansh Bhardwaj
+ * A simple "Hello, World!" program in Java.
+ * */
+
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+}
